901086e502e59857c53d7ea3478c8d6e68605602
dwnn <121233211+dwnmmsnn@users.noreply.github.com>
2024-11-20 10:48:13 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+document
+  .getElementById("submitbtn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const user = document.getElementById("lookup").value;
+    fetchGitHubProfile(user);
+  });
+
+async function fetchGitHubProfile(user) {
+  const profileDiv = document.getElementById("profileDiv");
+  profileDiv.innerHTML = "";
+
+  try {
+    const response = await fetch(`https://api.github.com/users/${user}`);
+    if (!response.ok) throw new Error("Profile not found");
+
+    const profileData = await response.json();
+    displayProfile(profileData);
+  } catch (error) {
+    profileDiv.innerHTML = `<p>${error}</p>`;
+  }
+}
+
+function displayProfile(profile) {
+  const profileHTML = `
+    <img src="${profile.avatar_url}" alt="Profile Image">
+    <h>${profile.name || "No name provided"}</h>
+    <div>
+      <p>User: ${profile.login}</p>
+      <p>Followers: ${profile.followers}</p>
+      <p>Public Repositories: ${profile.public_repos}</p>
+      <a href="${
+        profile.html_url
+      }" target="_blank"><button class="linkbtn">Visit Profile</button></a>
+    </div>
+  `;
+  document.getElementById("profileDiv").innerHTML = profileHTML;
+}
